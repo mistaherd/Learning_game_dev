e6b5e7664e8fdd3b6f85087ca975704fd124c629
mistaherd <lanzbulldog22@gmail.com>
2025-07-05 18:14:07 +0100

    renamed project name and  added test scene .made changes to player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,15 @@
 class_name Player extends CharacterBody2D
 const speed=800
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("Player_walk_left","Player_walk_right")
-	if direction!=0:
-		velocity.x=direction*speed
+	var direction_x := Input.get_axis("Player_walk_left","Player_walk_right")
+	var direction_y:=Input.get_axis("Player_walk_up","Player_walk_down")
+	
+	var Input_direction=Vector2(direction_x,direction_y)
+	if Input_direction.length()>0:
+		velocity=Input_direction.normalized()*speed
+		
 	else:
 		velocity.x=move_toward(velocity.x,0,speed)
+		velocity.y=move_toward(velocity.y,0,speed)
+	# enables collsions	
 	move_and_slide()
