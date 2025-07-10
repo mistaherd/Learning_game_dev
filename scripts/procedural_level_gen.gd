d70fe8612229f4deaec82d9e55204d7b77582a1e
mistaherd <lanzbulldog22@gmail.com>
2025-07-10 21:03:43 +0100

    added procudrual gen

--- scripts/procedural_level_gen.gd
+++ scripts/procedural_level_gen.gd
@@ -8,36 +8,29 @@ var width:int=100
 var height:int=100
 enum Tile_sets_SourceID {Desert_A1,Desert_A2,Desert_A5,Desert_Treerock}
 # added enum for visalbe layers feel free to add more  depend on how we want to implment it 
-enum Visable_layer {Visable_l0,Visable_L1}
-@onready var source_id :Tile_sets_SourceID=Tile_sets_SourceID.Desert_A1:set=set_source_id
-var water_atlas =Vector2(16,3)
+enum Visable_layer {Invisible,Visable_l0,Visable_L1}
+@onready var source_id :Tile_sets_SourceID=Tile_sets_SourceID.Desert_A1
+var water_atlas =Vector2(18,3)
 var land_atlas=Vector2(0,0)
 func _ready() -> void:
 	noise =noise_height_text.noise
 	generate_world()
 func generate_world():
-	for x in range(width):
-		for y in range(height):
+	for x in range(-width/2,width/2):
+		for y in range(-height/2,height/2):
 			var noise_val:float=noise.get_noise_2d(x,y)
 			if noise_val>=0.0:
 				#place land 
+			
 				source_id=Tile_sets_SourceID.Desert_A2
-				tile_map_layer.set_cell(Vector2(x,y),source_id,0)
-				pass
+				#print("Source id when placeing land =",source_id)
+				tile_map_layer.set_cell(Vector2(x,y),source_id,land_atlas)
+			
 			elif  noise_val<0.0:
 				#place water
-				source_id=Tile_sets_SourceID.Desert_A1
 				
-				pass
-## we want this for items 
-func set_source_id(state:Tile_sets_SourceID):
-	match state:
-		Tile_sets_SourceID.Desert_A1:
-			pass
-		Tile_sets_SourceID.Desert_A2:
-			pass
-		Tile_sets_SourceID.Desert_A5:
-			pass
-		Tile_sets_SourceID.Desert_Treerock:
-			pass
-	pass
+				source_id=Tile_sets_SourceID.Desert_A1
+				#print("source id when placeing water =",source_id)
+				print(Vector2(x,y),source_id,water_atlas)
+				tile_map_layer.set_cell(Vector2(x,y),source_id,water_atlas)
+	
