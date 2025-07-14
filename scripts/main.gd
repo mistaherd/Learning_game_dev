5c6f618307a77d2c86261368565f06138c9e3690
mistaherd <lanzbulldog22@gmail.com>
2025-07-14 02:01:30 +0100

    add basic world gen

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var score:float = 0.0
 @onready var time:float = 0.0
-
+@onready var PROCEDURAL_LEVEL_GEN:Produce_world = preload("res://scences/procedural_level_gen.tscn").instantiate()
 func Update_score(amount:float):#
 	score+=amount
 
@@ -12,4 +12,7 @@ func Update_score(amount:float):#
 
 func _process(delta: float) -> void:
 	time +=delta 
-	
+func _ready() -> void:
+	add_child(PROCEDURAL_LEVEL_GEN)
+	PROCEDURAL_LEVEL_GEN.generate_world()
+	pass
--- scripts/procedural_level_gen.gd
+++ scripts/procedural_level_gen.gd
@@ -1,36 +1,53 @@
-extends Node2D
+class_name Produce_world extends Node2D
 
 @export var noise_height_text:NoiseTexture2D
-@onready var tile_map_layer: TileMapLayer = $TileMapLayer
+@onready var water: TileMapLayer = $water
+@onready var ground_1: TileMapLayer = $ground1
+@onready var ground_2: TileMapLayer = $ground2
+@onready var clif: TileMapLayer = $Clif
+@onready var enviroment: TileMapLayer = $enviroment
+
 
 var noise:Noise
-var width:int=100
-var height:int=100
+@export var width:int=100
+@export var height:int=100
 enum Tile_sets_SourceID {Desert_A1,Desert_A2,Desert_A5,Desert_Treerock}
 # added enum for visalbe layers feel free to add more  depend on how we want to implment it 
 enum Visable_layer {Invisible,Visable_l0,Visable_L1}
 @onready var source_id :Tile_sets_SourceID=Tile_sets_SourceID.Desert_A1
-var water_atlas =Vector2(18,3)
-var land_atlas=Vector2(0,0)
+@onready var water_atlas_location =Vector2(18,3)
+@onready var sand_atlas_location=Vector2(0,0)
+@onready var grass_atlas_location=Vector2(10,3)
+@onready var clif_atlas_location=Vector2(0,20)
+
 func _ready() -> void:
 	noise =noise_height_text.noise
-	generate_world()
+	#generate_world()
 func generate_world():
 	for x in range(-width/2,width/2):
 		for y in range(-height/2,height/2):
 			var noise_val:float=noise.get_noise_2d(x,y)
+			#tree noise val here
 			if noise_val>=0.0:
-				#place land 
-			
 				source_id=Tile_sets_SourceID.Desert_A2
-				#print("Source id when placeing land =",source_id)
-				tile_map_layer.set_cell(Vector2(x,y),source_id,land_atlas)
-			
-			elif  noise_val<0.0:
-				#place water
+				ground_1.set_cell(Vector2(x,y),source_id,sand_atlas_location)
+				#place land 
+				if noise_val >0.25:
+					#if noise_val <0.35 :
+					## place clouds/ tress in here in here		
+					#print("Source id when placeing land =",source_id
+					source_id=Tile_sets_SourceID.Desert_A2
+					ground_1.set_cell(Vector2(x,y),source_id,grass_atlas_location)
 				
-				source_id=Tile_sets_SourceID.Desert_A1
-				#print("source id when placeing water =",source_id)
-				print(Vector2(x,y),source_id,water_atlas)
-				tile_map_layer.set_cell(Vector2(x,y),source_id,water_atlas)
+				if noise_val >0.4:
+					source_id=Tile_sets_SourceID.Desert_A5
+					ground_2.set_cell(Vector2(x,y),source_id,clif_atlas_location)
+					pass
+		
+			#place water
+				
+			source_id=Tile_sets_SourceID.Desert_A1
+			#print("source id when placeing water =",source_id)
+			print(Vector2(x,y),source_id,water_atlas_location)
+			water.set_cell(Vector2(x,y),source_id,water_atlas_location)
 	
